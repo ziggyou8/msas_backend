171573268c82a6f24abf64725fd8d875dfd156bd
Khadim Gaye <khadim.gaye@outlook.com>
2022-03-18 16:35:06 +0000

    Correction au niveau des users

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers\API;
-use Illuminate\Http\Request;
+
 use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
@@ -11,9 +12,8 @@ use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends BaseController
 {
-    public function __construct()                           
+    public function __construct()
     {
-       // $this->middleware("cors");
     }
     /**
      * Register api
@@ -21,7 +21,7 @@ class RegisterController extends BaseController
      * @return \Illuminate\Http\Response
      */
 
-     /**
+    /**
      * @OA\Post(
      ** path="/v1/register",
      *   tags={"Auth"},
@@ -68,7 +68,7 @@ class RegisterController extends BaseController
      *           type="string"
      *      )
      *   ),
-     * 
+     *
      *   @OA\Parameter(
      *      name="c_password",
      *      in="query",
@@ -111,31 +111,31 @@ class RegisterController extends BaseController
             "password" => "required",
             "c_password" => "required|same:password",
         ]);
-   
-        if($validator->fails()){
-            return $this->sendError("Validation Error.", $validator->errors());       
+
+        if ($validator->fails()) {
+            return $this->sendError("Validation Error.", $validator->errors());
         }
-   
+
         $input = $request->all();
         $input["password"] = bcrypt($input["password"]);
         $user = User::create($input);
-        $success["token"] =  $user->createToken("MyApp")->accessToken;
-        $success["id"] =  $user->id;
-        $success["prenom"] =  $user->prenom;
-        $success["nom"] =  $user->nom;
-        $success["email"] =  $user->email;
-        $success["telephone"] =  $user->telephone;
-   
+        $success["token"] = $user->createToken("MyApp")->accessToken;
+        $success["id"] = $user->id;
+        $success["prenom"] = $user->prenom;
+        $success["nom"] = $user->nom;
+        $success["email"] = $user->email;
+        $success["telephone"] = $user->telephone;
+
         return $this->sendResponse($success, "User register successfully.");
     }
-   
+
     /**
      * Login api
      *
      * @return \Illuminate\Http\Response
      */
 
-     /**
+    /**
      * @OA\Post(
      ** path="/v1/login",
      *   tags={"Auth"},
@@ -185,21 +185,20 @@ class RegisterController extends BaseController
      **/
     public function login(Request $request)
     {
-        if(Auth::attempt(["email" => $request->email, "password" => $request->password]) && Auth::user()->actif){ 
-            $user = Auth::user(); 
-            $success["token"] =  $user->createToken("MyApp")->accessToken;
-            $success["id"] =  $user->id;
-            $success["prenom"] =  $user->prenom;
-            $success["nom"] =  $user->nom;
-            $success["email"] =  $user->email;
-            $success["telephone"] =  $user->telephone;
-            $success["roles"] =  $user->getRoleNames();
-   
+        if (Auth::attempt(["email" => $request->email, "password" => $request->password]) && Auth::user()->actif) {
+            $user = Auth::user();
+            $success["token"] = $user->createToken("MyApp")->accessToken;
+            $success["id"] = $user->id;
+            $success["prenom"] = $user->prenom;
+            $success["nom"] = $user->nom;
+            $success["email"] = $user->email;
+            $success["telephone"] = $user->telephone;
+            $success["roles"] = $user->getRoleNames();
+
             return $this->sendResponse($success, "User login successfully.");
-        } 
-        else{ 
-            return $this->sendError("Unauthorised.", ["error"=>"Unauthorised"]);
-        } 
+        } else {
+            return $this->sendError("Unauthorised.", ["error" => "Unauthorised"]);
+        }
     }
 
 
@@ -238,9 +237,10 @@ class RegisterController extends BaseController
      *)
      **/
 
-        public function logout (Request $request) {
-            $token = $request->user()->token();
-            $token->revoke();
-            return response(["message" => "Deconnection avec succés"], 200);
-        }
+    public function logout(Request $request)
+    {
+        $token = $request->user()->token();
+        $token->revoke();
+        return response(["message" => "Deconnection avec succés"], 200);
+    }
 }
