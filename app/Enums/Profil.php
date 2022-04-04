2893875fbdcf8d2f9046ef2e3549a91626572b2e
Khadim Gaye <khadim.gaye@outlook.com>
2022-04-04 21:21:11 +0000

    Correction

--- app/Enums/Profil.php
+++ app/Enums/Profil.php
@@ -8,4 +8,18 @@ final class Profil extends Enum
 {
     const Chercheur = "chercheur";
     const Medecin = "medecin";
+
+    public static function getDescription($value): string
+    {
+        switch ($value) {
+            case self::Chercheur:
+                return 'Chercheur';
+                break;
+            case self::Medecin:
+                return 'Medecin';
+                break;
+            default:
+                return $value;
+        }
+    }
 }
